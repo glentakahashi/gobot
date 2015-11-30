a86f123104cdad6ffd113147ebd60583edb36121
Glen Takahashi <me@glentaka.com>
2015-11-30 00:06:50 -0500

    More features

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python
+from collections import deque
 import argparse
 import urllib2
 from bs4 import BeautifulSoup
@@ -9,21 +10,24 @@ import unicodecsv
 
 parser = argparse.ArgumentParser(description='Scrape one of the ___lounge.com sites.')
 parser.add_argument('site', metavar='site', type=str, help='which site to parse, either dota2lounge or csgolounge')
+parser.add_argument('--retry-failed', type=bool, metavar='failed', help='retry failed')
 
 args = parser.parse_args()
 site = args.site
 
 LOUNGE_URL = "http://%slounge.com/"%site
 
-csgocurrent = 7473
-dota2current = 10499
-
 current = 0
 
-if site == 'csgo':
-    current = csgocurrent
-elif site == 'dota2':
-    current = dota2current
+req = urllib2.Request(LOUNGE_URL, headers={'User-Agent' : "scraping bot"})
+con = urllib2.urlopen(req)
+html = con.read()
+soup = BeautifulSoup(html, 'html.parser')
+matches = soup.select("div.match > div > a")
+for match in matches:
+    match = int(re.findall(r'\d+', match['href'])[0])
+    if match > current:
+        current = match
 
 def getMatch(url, i):
     req = urllib2.Request(url + str(i), headers={'User-Agent' : "scraping bot"})
@@ -33,6 +37,9 @@ def getMatch(url, i):
     soup = BeautifulSoup(html, 'html.parser')
     if len(soup.select("body > main > section > h1")) > 0 and soup.select("body > main > section > h1")[0].text == '404':
         print ("404, matchid = " + str(i))
+        with open('%s.csv'%site, 'ab') as csvfile:
+            writer = unicodecsv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+            writer.writerow([i, "404"])
         return
     date = soup.select("body > main > section:nth-of-type(1) > div.box-shiny-alt > div:nth-of-type(1) > div:nth-of-type(3)")[0]
     date = date['title'] + " " + date.text.strip()
@@ -46,19 +53,40 @@ def getMatch(url, i):
     team_a_won = "(win)" in team_a.lower()
     team_b_won = "(win)" in team_b.lower()
     status = soup.select("body > main > section:nth-of-type(1) > div.box-shiny-alt > div:nth-of-type(2)")[0]
-    if 'class' not in status or 'full' in status['class']:
+    if status.has_attr('class') and 'full' in status['class']:
         status = ""
     else:
-        status = status.text
+        status = status.text.strip()
     winner = "a" if team_a_won else "b" if team_b_won else "none"
 
     with open('%s.csv'%site, 'ab') as csvfile:
         writer = unicodecsv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
         writer.writerow([i, date, bo, team_a, team_b, team_a_pct, team_b_pct, team_a_odds, team_b_odds, winner, status])
 
-# loops over pages (-> 8800 all matches in 2014 & 2015 at 27.11.2015)
-for i in range(1,current+1):
-    url = LOUNGE_URL + "match?m="
-    getMatch(url, i)
-    #writeMatchesToFile(matchesOfPage, i)
-    #print strftime("%Y-%m-%d %H:%M:%S: ", gmtime()) + str(i + 50) + " HLTV CS:GO matches completed."
+start = 1
+url = LOUNGE_URL + "match?m="
+
+with open('%s.csv'%site, 'rb') as csvfile:
+    reader = unicodecsv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    start = int(deque(reader,1)[0][0])+1
+
+if args.retry_failed:
+    with open('%s.csv'%site, 'rb') as csvfile:
+        reader = unicodecsv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        for row in reader:
+            if row[1] == 'failed' or row[1] == '404':
+                try:
+                    getMatch(url,int(row[0]))
+                except Exception as e:
+                    print "%s still failed"%row[0]
+else:
+    for i in range(start,current+1):
+        try:
+            getMatch(url, i)
+        except Exception as e:
+            print(str(i) +  " failed parsing")
+            with open('%s.csv'%site, 'ab') as csvfile:
+                writer = unicodecsv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+                writer.writerow([i, "failed"])
+        #writeMatchesToFile(matchesOfPage, i)
+        #print strftime("%Y-%m-%d %H:%M:%S: ", gmtime()) + str(i + 50) + " HLTV CS:GO matches completed."
---
+++ scraper.py
@@ -0,0 +1,55 @@
+#!/usr/local/bin/python
+# scrape HLTV results for CS:GO Matches
+
+# get website content
+import urllib2
+from bs4 import BeautifulSoup
+import re
+import csv
+from time import gmtime, strftime
+import unicodecsv
+
+# extract match information and format to list
+def formatMatch(hltvMatch):
+   hltvMatchNames = hltvMatch.get_text(";", strip = True)
+   hltvMatchScore = re.findall('\((\d*?)\)', hltvMatchNames)
+   hltvMatchIds = [re.search('(teamid|matchid|eventid)=?(\d*)', link.get('href')).group(2) for link in hltvMatch.select("a")]
+   hltvMatchLine = hltvMatchNames.split(';') + hltvMatchIds + hltvMatchScore
+   return hltvMatchLine
+
+# gets all matches from one page
+def getMatchesOfPage(hltvUrl):
+   hltvReq = urllib2.Request(hltvUrl, headers={'User-Agent' : "github users please insert something meaningful here"})
+   hltvCon = urllib2.urlopen(hltvReq)
+   hltvHTML = hltvCon.read()
+   hltvSoup = BeautifulSoup(hltvHTML, 'html.parser')
+
+   hltvMatches = hltvSoup.select("#back > div.mainAreaNoHeadline > div.centerNoHeadline > div > div.covMainBoxContent > div > div > div")[5:]
+   hltvMatchesFormatted = [formatMatch(hltvMatch) for hltvMatch in hltvMatches]
+
+   return hltvMatchesFormatted
+
+# writes lists to file
+def writeMatchesToFile(matchesOfPage, iteration):
+   with open('hltv_org_matches_2014.csv', 'ab') as csvfile:
+      hltvWriter = unicodecsv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+      if iteration == 0:
+         hltvWriter.writerow(["date", "team1", "team2", "map", "event", "matchid", "teamid1", "teamid2", "eventid", "score1", "score2"])
+      for match in matchesOfPage:
+         hltvWriter.writerow(match)
+
+# loops over pages (-> 8800 all matches in 2014 & 2015 at 27.11.2015)
+for i in range(0, 12000, 50):
+   hltvUrlbase = 'http://www.hltv.org/?pageid=188&statsfilter=0&offset='
+   hltvUrlOffset = str(i)
+   hltvUrl = hltvUrlbase + hltvUrlOffset
+   matchesOfPage = getMatchesOfPage(hltvUrl)
+   writeMatchesToFile(matchesOfPage, i)
+   print strftime("%Y-%m-%d %H:%M:%S: ", gmtime()) + str(i + 50) + " HLTV CS:GO matches completed."
+
+
+
+
+
+
+
